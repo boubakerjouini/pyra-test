a4d7672e35cc28009dc9b4c677e3c5d2b61a1233
JOUINI Boubaker <boubaker.jouini@esprit.tn>
2022-05-09 19:20:31 +0100

    added Header & react router

---
+++ src/Components/App/index.js
@@ -0,0 +1,15 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+import Header from '../Header'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+
+      <Typography variant="h2">Pyra App</Typography>
+    </>
+  )
+}
+
+export default App
